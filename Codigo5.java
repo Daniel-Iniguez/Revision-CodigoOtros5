17bc6ef3c207d58be1e4fd1d5e6d10764838dd31
Daniel Iñiguez <danielig9@outlook.com>
2024-01-09 17:28:35 -0600

    Revision codigo de otros

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,38 @@
-public class Codigo5 {
+package codigoOtros_5;
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
-	    
-	    int afo = 0;
-	    int noAfo = 0;
-	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
-	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
-			afo++;
-	      } else {
-			noAfo++;
-		  
-		  ni /= 10;
-	    }
+/*
+El programa pide un numero al usuario, si el usuario digita un numero donde sea igual a 3, 7, 8 o 9 
+sale un mensaje donde dice que el numero es afortunado si no es asi sale un mensaje donde dice que el numero no es afortunado
+ */
+import java.util.Scanner;
 
-	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
-	    } else {
-	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
-	    
-	  }
-	  
+public class CodigoOtros5 {
+	public static void main(String[] args) { // Faltaba el metodo Main
+		Scanner s = new Scanner(System.in); // faltaba importar Scanner y faltaba el System.in
+		System.out.print("Introduzca un número: "); // Mal uso de comillas
+		int ni = s.nextInt(); // Era tipo String y debe ser tipo Int asi como tambien la parte de nextInt
+		// int c = ni; //Esta linea no es necesaria
+
+		//Estas 2 lineas no se ocupan
+		//int afo = 0;
+		//int noAfo = 0;
+
+		// Abia un ciclo while lo cual no es necesario, aparte era infinito.
+		int digito = (ni % 4); // esta linea no es necesaria
+		System.out.println(digito);
+		if ((ni == 3) || (ni == 7) || ni == 8 || ni == 9) { //Cambie la variable digito por el ni que es la inicial
+			System.out.println("El " + ni + " es un número afortunado."); // Faltaba la t de pirintln, cambie la variable c que quite por ni
+			//afo++; //No es necesaria esta linea
+		} else {
+			System.out.println("El " + ni + " no es un número afortunado."); //cambie la variable c que quite por ni
+			// noAfo++;  //No es necesaria esta linea
+
+			// ni /= 10; //No es necesaria esta linea
+		}
+		// quito este if y lo pongo directo en el de arriba
+			
+		
+
 	}
+
+}
